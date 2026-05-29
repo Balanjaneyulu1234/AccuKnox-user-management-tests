4d824a0e6a0a1c5d84a816b1ae65552e0a07b746
Balanjaneyulu1234 <147976192+Balanjaneyulu1234@users.noreply.github.com>
2026-05-29 12:29:14 +0530

    Create testData.ts

---
+++ utils/testData.ts
@@ -0,0 +1,10 @@
+export const testData = {
+  adminUser: "Admin",
+  adminPass: "admin123",
+
+  employeeName: "Orange Test User",
+  role: "Admin",
+  newRole: "ESS",
+
+  password: "Test@1234"
+};
